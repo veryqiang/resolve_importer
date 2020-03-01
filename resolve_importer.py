e5feb74e6984491f5ddb709b788e3aa24ac44e44
veryqiang <veryqiang@gmail.com>
2020-03-01 22:10:12 +0800

    new feature to set clip color and clip marker with notes; a new UI;

--- resolve_importer.py
+++ resolve_importer.py
@@ -66,7 +66,7 @@ def get_all_subfolders(mp_folder):
     return folderlist
 
 
-def make_timeline_with_folder(mp_folder, notes):
+def make_timeline_with_folder(mp_folder, notes, timeline_notes_flag):
     """make a timeline with all files inside the 'mp_folder' object;
     Keyword Arguments:
         mp_folder - Resolve media pool folder object
@@ -74,7 +74,8 @@ def make_timeline_with_folder(mp_folder, notes):
     mp_folder_name = mp_folder.GetName()
     # initial timeline and marker
     new_timeline = mp.CreateTimelineFromClips(mp_folder_name, list(mp_folder.GetClips().values()))
-    new_timeline.AddMarker(new_timeline.GetEndFrame() - new_timeline.GetStartFrame(),
+    if timeline_notes_flag:
+        new_timeline.AddMarker(new_timeline.GetEndFrame() - new_timeline.GetStartFrame(),
                            'Green', mp_folder_name, notes, 1)
     # work on the subfolders, add clips inside subfolder to timeline, and maker at the end
     subfolder_list = get_all_subfolders(mp_folder)
@@ -83,61 +84,100 @@ def make_timeline_with_folder(mp_folder, notes):
         for clip in subfolder.GetClips().values():
             clips_in_this_subfolder.append(clip)
         mp.AppendToTimeline(clips_in_this_subfolder)
-        new_timeline.AddMarker(new_timeline.GetEndFrame() - new_timeline.GetStartFrame(),
+        if timeline_notes_flag:
+            new_timeline.AddMarker(new_timeline.GetEndFrame() - new_timeline.GetStartFrame(),
                                'Green', subfolder.GetName(), notes, 1)
 
-# TODO GUI need some design
-layout = [[sg.Text('Import footage from folder', font=('Default', 16), pad=(20, 10)),
-           sg.Button('Browse', pad=(40, 10), font=('Default', 15), size=(6, 1), key='Import')],
-          [sg.Text('Tags', pad=(20, 10)), sg.Checkbox('CAMERA', key='CAMERA'),
-           sg.Checkbox('VFX', key='VFX'),
-           sg.Checkbox('GRAPHIC', key='GRAPHIC'),
-           sg.Checkbox('REF', key='REF')],
-          [sg.Text('Filter', pad=(20, 10)), sg.Radio('File', "RADIO1", key='FileOnly'),
-           sg.Radio('Folder', "RADIO1", key='FolderOnly'),
-           sg.Radio('Both', "RADIO1", key='FolderAndFiles', default=True)],
-          [sg.Checkbox('Timeline', key='TIMELINE', pad=(20, 10), default=True),
-           sg.Multiline('Input notes...',key='NOTES', size=(36,1), focus=True)],
-          [sg.Cancel('Exit', font=('Default', 12), pad=(20, 15))]]
 
-window = sg.Window('Resolve Import v0.2a', layout)
+def get_cliplist_in_folder(mp_folder):
+    """return a list containing all clips inside folder"""
+    cliplist = []
+    cliplist += mp_folder.GetClips().values()
+    subfolder_list = get_all_subfolders(mp_folder)
+    for subfolder in subfolder_list:
+        cliplist += subfolder.GetClips().values()
+    return cliplist
 
-while True:
-    # read PySimpleGUI window
-    event, values = window.read()
-    input_path = ''
-    tags = ('CAMERA', 'VFX', 'GRAPHIC', 'REF')
 
-    # if user clicks Exit
-    if event in ('Exit', None):
-        break
+def make_gui():
+    sg.theme('DarkBlue9')
+    sg.SetOptions(font=('default', 12))
 
-    # if user clicks Browse
-    elif event == "Import":
-        input_path = sg.popup_get_folder('Footage path', font=("Helvetica", 16))
-        if input_path:
-            # assemble the tags
-            checked_tags = [key for key, val in values.items() if (val and (key in tags))]
-            if not checked_tags:
-                checked_tags = ['untagged']
-            # add the very first folder in media pool
-            folder_name = f"{datetime.datetime.now():%Y%m%d_%H%M%S_}" + '_'.join(checked_tags) + '-' + \
-                          os.path.split(input_path)[1]
-            imported_mp_folder = mp.AddSubFolder(mp.GetRootFolder(), folder_name)
-            # no folder structure
-            if values['FileOnly']:
-                ms.AddItemsToMediaPool(input_path)
-            else:
-                if values['FolderOnly']:
-                    add_files_flag = 0
+    frame_import = [
+        [sg.Text('Tags', pad=(20, 10)), sg.Checkbox('CAMERA', key='CAMERA'),
+         sg.Checkbox('VFX', key='VFX'),
+         sg.Checkbox('GRAPHIC', key='GRAPHIC'),
+         sg.Checkbox('REF', key='REF')],
+        [sg.Text('Filter', pad=(20, 10)), sg.Radio('File', "RADIO1", key='FileOnly'),
+         sg.Radio('Folder', "RADIO1", key='FolderOnly'),
+         sg.Radio('Both', "RADIO1", key='FolderAndFiles', default=True),
+         sg.Button('Browse', pad=(30, 10), font=('Default', 15), size=(6, 1), key='Import')]]
+
+    fram_timeline = [[sg.Checkbox('Timeline Notes', key='TIMELINENOTES',  pad=(0,10), default=False),
+                      sg.Checkbox('Create Timeline', key='TIMELINE', default=True, pad=((150,10),10))],
+                       [sg.InputText('Timeline notes...', text_color='pink', key='TNOTES', size=(48, 1),
+                                     pad=(5,(5,15)))]]
+
+    frame_clips = [[sg.Checkbox('Clip Notes', key='CLIPNOTES',  pad=(0,10), default=False),
+                    sg.Combo(values=['Orange', 'Apricot', 'Yellow', 'Lime', 'Olive', 'Green', 'Teal', 'Navy', 'Blue',
+                                      'Purple', 'Violet', 'Pink', 'Tan', 'Beige', 'Brown', 'Chocolate'],
+                              default_value='Blue(Default)', key='CLIPCOLOR', size=(15,1),pad=((80,10),10)),
+                    sg.Text('Clip Color', pad=(0,10))],
+                    [sg.InputText('Clip notes...', text_color='pink', key='CNOTES', size=(48, 1), pad=(5,(5,15)))]]
+
+    layout = [[sg.Frame('Import footage from folder', frame_import, font=('Default', 18), pad=(30, (15,0)))],
+              [sg.Frame('Clip options', frame_clips, font=('Default', 18), pad=(30, 10))],
+              [sg.Frame('Timline options', fram_timeline, font=('Default', 18), pad=(30, 10))],
+              [sg.Cancel('Exit', size=(6,1), font=('Default', 15), pad=(30, 15))]]
+
+    window = sg.Window('Resolve Import v0.2a', layout)
+
+    while True:
+        # read PySimpleGUI window
+        event, values = window.read()
+        input_path = ''
+        tags = ('CAMERA', 'VFX', 'GRAPHIC', 'REF')
+
+        # if user clicks Exit
+        if event in ('Exit', None):
+            break
+
+        # if user clicks Browse
+        elif event == "Import":
+            input_path = sg.popup_get_folder('Footage path', font=("Helvetica", 16))
+            if input_path:
+                # assemble the tags
+                checked_tags = [key for key, val in values.items() if (val and (key in tags))]
+                if not checked_tags:
+                    checked_tags = ['untagged']
+                # add the very first folder in media pool
+                folder_name = f"{datetime.datetime.now():%Y%m%d_%H%M%S_}" + '_'.join(checked_tags) + '-' + \
+                              os.path.split(input_path)[1]
+                imported_mp_folder = mp.AddSubFolder(mp.GetRootFolder(), folder_name)
+                # no folder structure
+                if values['FileOnly']:
+                    ms.AddItemsToMediaPool(input_path)
                 else:
-                    add_files_flag = 1
-                mp_add_source(input_path, add_files_flag)
+                    if values['FolderOnly']:
+                        add_files_flag = 0
+                    else:
+                        add_files_flag = 1
+                    mp_add_source(input_path, add_files_flag)
 
-            if values['TIMELINE']:
-                mp.SetCurrentFolder(imported_mp_folder)
-                make_timeline_with_folder(imported_mp_folder, values['NOTES'])
+                for a_clip in get_cliplist_in_folder(imported_mp_folder):
+                    if values['CLIPNOTES']:
+                        a_clip.AddMarker(0, 'Green', folder_name, values['CNOTES'], 1)
+                    if values['CLIPCOLOR']!='Blue(Default)':
+                        a_clip.SetClipColor(values['CLIPCOLOR'])
 
-            sg.popup('done!')
+                if values['TIMELINE']:
+                    mp.SetCurrentFolder(imported_mp_folder)
+                    make_timeline_with_folder(imported_mp_folder, values['TNOTES'], values['TIMELINENOTES'])
 
-window.close()
+                sg.popup('done!')
+
+    window.close()
+
+
+if __name__ == '__main__':
+    make_gui()
